f1da6d987a5b4f145bcdf297e5df047ab453ad4f
irakli-kupatadze <iraklikupatadze1324999@gmail.com>
2026-03-24 14:45:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 import requests
 import json
 
-time_range = "2m"
-latitude_from_user=input(("please input the latitude of your location: "))
-longitude_from_user=input(("please input the longitude of your location: "))
-latitude_for_spain="40.00000000"
-longitude_from_spain="-4.00000000"
-latitude_for_germany="51.00000000"
-longitude_from_germany="9.00000000"
-api_url =f"https://api.open-meteo.com/v1/forecast?latitude={latitude_from_user}&longitude={longitude_from_user}&hourly=temperature_{time_range}"
-response = requests.get(api_url)
-print(response)
+locations = [
+    ("Germany (Hesse)", 51.00000000, 9.00000000),
+    ("Spain (Castilla-La Mancha)", 40.00000000, -4.00000000)
+]
 
-if response.status_code == 200:
-    fild_name= f"temperature_{time_range}"
-    print("Request successful!")
-    data = response.json()
-    for temp in data["hourly"][fild_name]:
-        print("your location temp in 2 minute intervals " , temp)
-else:
-    print(f"Request failed with status code: {response.status_code}")
-    print(f"Error details: {response.text}")
+def get_weather(latitude, longitude):
+    api_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
+    response = requests.get(api_url)
 
-api_url =f"https://api.open-meteo.com/v1/forecast?latitude={latitude_for_spain}&longitude={longitude_from_spain}&hourly=temperature_{time_range}"
-response = requests.get(api_url)
-print("spain location", response)
-if response.status_code == 200:
-    fild_name= f"temperature_{time_range}"
-    data = response.json()
-    print("spain temp", data["hourly"][fild_name])
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.status_code)
+        return None
 
-else:
-    print(f"Request failed with status code: {response.status_code}")
-    print(f"Error details: {response.text}")
+def display_temperatures(data):
+    if data is None:
+        print("No data available.")
+        return
+
+    temperatures = data["hourly"]["temperature_2m"]
+
+    print("\nTemperatures:")
+    for temp in temperatures:  # iteration
+        if temp is not None:   # selection
+            print(temp)
+
+print("Choose a location:")
+for i in range(len(locations)):
+    print(f"{i+1}: {locations[i][0]}")
+
+while True:
+    try:
+        choice = int(input("Enter number: "))
+        if 1 <= choice <= len(locations):
+            break
+        else:
+            print("Invalid choice. Try again.")
+    except:
+        print("Please enter a valid number.")
+
+selected_location = locations[choice - 1]
+name, lat, lon = selected_location
+
+print(f"\nYou selected: {name}")
 
 
-
-
-
-
-
+weather_data = get_weather(lat, lon)
+display_temperatures(weather_data)
